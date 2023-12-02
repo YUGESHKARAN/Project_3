c1d61b24905091df3278df4f2bd9b4c48c7703ce
YUGESHKARAN <yugeshkaran01@gmail.com>
2023-12-02 22:38:45 +0530

    all files shipped

---
+++ javas.js
@@ -0,0 +1,182 @@
+const sidebar = document.querySelector('.sidebar');
+function menubar(){
+    sidebar.style.display='flex'
+}
+
+
+
+
+
+const closenav = document.querySelector('.sidebar');
+function closebar(){
+    closenav.style.display='none'
+}
+
+
+const btnLogin = document.querySelector('.form-filed ')
+function show(){
+    btnLogin.style.display='block'
+    closenav.style.display='none'
+}
+
+const formField = document.querySelector('.form-filed')
+function loginclose(){
+    formField.style.display='none';
+    
+}
+
+
+const likebn = document.querySelector('.likebn')
+function like(){
+    likebn.style.fill='blue'
+  
+
+   
+}
+
+const fav=document.querySelector('.fav')
+function favourite(){
+    fav.style.fill='red'
+  
+    
+}
+
+
+
+
+
+
+const likeb1 = document.querySelector('.likeb1')
+function like1(){
+    likeb1.style.fill='blue'
+    
+   
+}
+const fav1=document.querySelector('.fav1')
+function favourite1(){
+    fav1.style.fill='red'
+   
+}
+
+
+
+
+const likeb2 = document.querySelector('.likeb2')
+function like2(){
+    likeb2.style.fill='blue'
+   
+}
+const fav2=document.querySelector('.fav2')
+function favourite2(){
+    fav2.style.fill='red'
+   
+}
+
+
+
+
+
+
+const likeb3 = document.querySelector('.likeb3')
+function like3(){
+    likeb3.style.fill='blue'
+   
+}
+const fav3=document.querySelector('.fav3')
+function favourite3(){
+    fav3.style.fill='red'
+   
+}
+
+const likeb4 = document.querySelector('.likeb4')
+function like4(){
+    likeb4.style.fill='blue'
+   
+}
+const fav4=document.querySelector('.fav4')
+function favourite4(){
+    fav4.style.fill='red'
+   
+}
+
+const likeb5 = document.querySelector('.likeb5')
+function like5(){
+    likeb5.style.fill='blue'
+   
+}
+const fav5=document.querySelector('.fav5')
+function favourite5(){
+    fav5.style.fill='red'
+   
+}
+
+const likeb6 = document.querySelector('.likeb6')
+function like6(){
+    likeb6.style.fill='blue'
+   
+}
+const fav6=document.querySelector('.fav6')
+function favourite6(){
+    fav6.style.fill='red'
+   
+}
+
+
+const likeb7 = document.querySelector('.likeb7')
+function like7(){
+    likeb7.style.fill='blue'
+   
+}
+const fav7=document.querySelector('.fav7')
+function favourite7(){
+    fav7.style.fill='red'
+   
+}
+
+
+const likeb8 = document.querySelector('.likeb8')
+function like8(){
+    likeb8.style.fill='blue'
+   
+}
+const fav8=document.querySelector('.fav8')
+function favourite8(){
+    fav8.style.fill='red'
+   
+}
+
+
+const likeb9 = document.querySelector('.likeb9')
+function like9(){
+    likeb9.style.fill='blue'
+   
+}
+const fav9=document.querySelector('.fav9')
+function favourite9(){
+    fav9.style.fill='red'
+   
+}
+
+
+const likeb10 = document.querySelector('.likeb10')
+function like10(){
+    likeb10.style.fill='blue'
+   
+}
+const fav10=document.querySelector('.fav10')
+function favourite10(){
+    fav10.style.fill='red'
+   
+}
+
+
+const likeb11 = document.querySelector('.likeb11')
+function like11(){
+    likeb11.style.fill='blue'
+   
+}
+const fav11=document.querySelector('.fav11')
+function favourite11(){
+    fav11.style.fill='red'
+   
+}
